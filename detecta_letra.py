05578f6d2f8679d1095db0c13b60990cd6147b8e
IrwinCedeño <user@kiwi93.irwin>
2019-08-08 10:00:44 -0500

    listo

---
+++ detecta_letra.py
@@ -0,0 +1,13 @@
+z = input()
+l = len(z)
+y, i = 'e', l
+print('')
+
+while i > 0:
+    if z[l - i] == y:
+            print('existe e en la posicion:', l - i)
+    else:
+        print('no existe e en la posicion:', l - i)
+
+    i -= 1
+   
