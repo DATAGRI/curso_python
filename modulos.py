f66eece0b8dc463a25eac4e0ced050f76854efce
IrwinCedeño <user@kiwi93.irwin>
2019-08-10 13:34:41 -0500

    listo

---
+++ modulos.py
@@ -0,0 +1,8 @@
+#import cociente_par as cp  # ejecuta el modulo importado sin mas
+#import detecta_letra as dl  #  ejecuta el modulo importado sin mas
+
+#print (cp.d2)  # imprime el valor de la consante
+
+from detecta_letra import a
+
+
